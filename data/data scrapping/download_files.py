039856c93a531ba5a15b1373db7831cbde5488a0
Rodrigo De Lama <rodrigo.delama@icloud.com>
2025-03-18 12:11:37 +0100

    Various updates
    Updated:
    - tfg notebook cleanup
    - requirements
    - data download script
    - data for the latest days

--- data/data scrapping/download_files.py
+++ data/data scrapping/download_files.py
@@ -1,8 +1,13 @@
-# https://www.omie.es/es/file-download?parents%5B0%5D=marginalpdbc&filename=marginalpdbc_20230101.1
-# https://www.omie.es/es/file-download?parents%5B0%5D=marginalpdbc&filename=marginalpdbc_20230102.1
-
 import requests
-import os
+from datetime import datetime, timedelta
+
+# Usage instructions:
+    # Set file name to save the filenames to download
+    # Set start and end dates
+
+file_path = 'year.txt'
+start_date = datetime(2025, 2, 14)
+end_date = datetime(2025, 3, 18)
 
 def download_files(urls):
     for url in urls:
@@ -10,7 +15,8 @@ def download_files(urls):
         filename = url.split('=')[-1]
         
         # Send a GET request to download the file
-        response = requests.get(url)
+        # response = requests.get(url)
+        response = requests.get(url, verify=False) # I hate Zscaler and EY tech
         
         # Check if the request was successful
         if response.status_code == 200:
@@ -21,6 +27,8 @@ def download_files(urls):
         else:
             print(f"Failed to download {filename}")
 
+# Example address to build
+# https://www.omie.es/es/file-download?parents%5B0%5D=marginalpdbc&filename=marginalpdbc_20230102.1
 def read_filenames_and_compose_urls(file_path):
     base_url = "https://www.omie.es/es/file-download?parents%5B0%5D=marginalpdbc&filename="
     
@@ -33,8 +41,28 @@ def read_filenames_and_compose_urls(file_path):
     
     return urls
 
+# Compose filenames to download
+# def compose_filenames() -> str: # Si quiero un tipo especifico poner eso
+def compose_filenames():
+    # Set start and end dates
+    start_date = start_date
+    end_date = end_date
+    
+    # Generate the entries for the number of days comprehended
+    to_generate = (end_date - start_date).days + 1
+    entries = []
+    for i in range(0, to_generate):  # x days from start to end
+        date_entry = start_date + timedelta(days=i)
+        entries.append(f'marginalpdbc_{date_entry.strftime("%Y%m%d")}.1')
+    
+    # save to a .txt file
+    with open(file_path, 'w') as file:
+        for entry in entries:
+            file.write(entry + '\n')
+
 # Example usage
-file_path = 'year.txt'
+compose_filenames()
+
 urls = read_filenames_and_compose_urls(file_path)
 
 download_files(urls)
