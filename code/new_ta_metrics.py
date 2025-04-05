aa5c2dc2adff972d985258636914577ac6d55b96
Rodrigo De Lama <rodrigo.delama@icloud.com>
2025-04-05 15:48:00 +0200

    Started w/ mini models
    Modified metric ranges
    Added new tfg notebook for mini models - need to clean up the nb
    Cleanup needed for .py

--- code/new_ta_metrics.py
+++ code/new_ta_metrics.py
@@ -56,60 +56,38 @@ df_hour = df[df['Datetime'].dt.hour == hour_to_predict].copy()
 # Test with various rolling windows
 
 # Simple Moving Average (SMA) (shorter window)
-df_hour[f'SMA_{3}'] = ta.trend.SMAIndicator(close=df_hour['MarginalES'], window=3).sma_indicator()
-df_hour[f'SMA_{5}'] = ta.trend.SMAIndicator(close=df_hour['MarginalES'], window=5).sma_indicator()
-df_hour[f'SMA_{7}'] = ta.trend.SMAIndicator(close=df_hour['MarginalES'], window=7).sma_indicator()
+df_hour[f'SMA_{3}'] = ta.trend.SMAIndicator(close=df_hour['MarginalES'], window=3).sma_indicator() # last few days
+df_hour[f'SMA_{5}'] = ta.trend.SMAIndicator(close=df_hour['MarginalES'], window=5).sma_indicator() # work week
+df_hour[f'SMA_{7}'] = ta.trend.SMAIndicator(close=df_hour['MarginalES'], window=7).sma_indicator() # 1 week
 
 # Simple Moving Average (longer window)
-df_hour[f'SMA_{10}'] = ta.trend.SMAIndicator(close=df_hour['MarginalES'], window=10).sma_indicator() # short term in trading
-df_hour[f'SMA_{30}'] = ta.trend.SMAIndicator(close=df_hour['MarginalES'], window=30).sma_indicator()
-df_hour[f'SMA_{50}'] = ta.trend.SMAIndicator(close=df_hour['MarginalES'], window=50).sma_indicator() # medium term in trading
-df_hour[f'SMA_{60}'] = ta.trend.SMAIndicator(close=df_hour['MarginalES'], window=60).sma_indicator()
-df_hour[f'SMA_{100}'] = ta.trend.SMAIndicator(close=df_hour['MarginalES'], window=100).sma_indicator()
-df_hour[f'SMA_{200}'] = ta.trend.SMAIndicator(close=df_hour['MarginalES'], window=200).sma_indicator() # long term in trading
+df_hour[f'SMA_{14}'] = ta.trend.SMAIndicator(close=df_hour['MarginalES'], window=14).sma_indicator() # 2 weeks
+df_hour[f'SMA_{30}'] = ta.trend.SMAIndicator(close=df_hour['MarginalES'], window=30).sma_indicator() # 1 month
+df_hour[f'SMA_{60}'] = ta.trend.SMAIndicator(close=df_hour['MarginalES'], window=60).sma_indicator() # 2 months
+df_hour[f'SMA_{90}'] = ta.trend.SMAIndicator(close=df_hour['MarginalES'], window=90).sma_indicator() # 3 months
+df_hour[f'SMA_{180}'] = ta.trend.SMAIndicator(close=df_hour['MarginalES'], window=180).sma_indicator() # 1/2 year
+df_hour[f'SMA_{360}'] = ta.trend.SMAIndicator(close=df_hour['MarginalES'], window=360).sma_indicator() # 1 year
 
 # Exponential Moving Average (EMA) (Rolling Window set as typically done in trading) - da mas peso a los precios recientes
-df_hour[f'EMA_{12}'] = ta.trend.EMAIndicator(close=df_hour['MarginalES'], window=12).ema_indicator()
-df_hour[f'EMA_{26}'] = ta.trend.EMAIndicator(close=df_hour['MarginalES'], window=26).ema_indicator()
+df_hour[f'EMA_{3}'] = ta.trend.EMAIndicator(close=df_hour['MarginalES'], window=3).ema_indicator()
+df_hour[f'EMA_{5}'] = ta.trend.EMAIndicator(close=df_hour['MarginalES'], window=5).ema_indicator()
+df_hour[f'EMA_{7}'] = ta.trend.EMAIndicator(close=df_hour['MarginalES'], window=7).ema_indicator()
+df_hour[f'EMA_{14}'] = ta.trend.EMAIndicator(close=df_hour['MarginalES'], window=14).ema_indicator()
+df_hour[f'EMA_{30}'] = ta.trend.EMAIndicator(close=df_hour['MarginalES'], window=30).ema_indicator()
 
 # Rate of Change (ROC, over N days)
+df_hour[f'ROC_{3}'] = ta.momentum.ROCIndicator(close=df_hour['MarginalES'], window=3).roc()
+df_hour[f'ROC_{5}'] = ta.momentum.ROCIndicator(close=df_hour['MarginalES'], window=5).roc()
+df_hour[f'ROC_{7}'] = ta.momentum.ROCIndicator(close=df_hour['MarginalES'], window=7).roc()
 df_hour[f'ROC_{12}'] = ta.momentum.ROCIndicator(close=df_hour['MarginalES'], window=12).roc() # as done in trading for momentum
-df_hour[f'ROC_{50}'] = ta.momentum.ROCIndicator(close=df_hour['MarginalES'], window=50).roc()
+df_hour[f'ROC_{14}'] = ta.momentum.ROCIndicator(close=df_hour['MarginalES'], window=14).roc()
+df_hour[f'ROC_{30}'] = ta.momentum.ROCIndicator(close=df_hour['MarginalES'], window=30).roc()
 
 #! Review if this is useful
 # Relative Strength Index (RSI, N-day window)
 df_hour[f'RSI_{5}'] = ta.momentum.RSIIndicator(close=df_hour['MarginalES'], window=5).rsi() # rapid momentum changes
 df_hour[f'RSI_{7}'] = ta.momentum.RSIIndicator(close=df_hour['MarginalES'], window=7).rsi() # rapid momentum changes
 df_hour[f'RSI_{14}'] = ta.momentum.RSIIndicator(close=df_hour['MarginalES'], window=14).rsi() # standard momentum
-
-# # Return
-# # Simple Moving Average (SMA) (shorter window)
-# df_hour[f'SMA_{3}'] = ta.trend.SMAIndicator(close=df_hour['Return'], window=3).sma_indicator()
-# df_hour[f'SMA_{5}'] = ta.trend.SMAIndicator(close=df_hour['Return'], window=5).sma_indicator()
-# df_hour[f'SMA_{7}'] = ta.trend.SMAIndicator(close=df_hour['Return'], window=7).sma_indicator()
-
-# # Simple Moving Average (longer window)
-# df_hour[f'SMA_{10}'] = ta.trend.SMAIndicator(close=df_hour['Return'], window=10).sma_indicator() # short term in trading
-# df_hour[f'SMA_{30}'] = ta.trend.SMAIndicator(close=df_hour['Return'], window=30).sma_indicator()
-# df_hour[f'SMA_{50}'] = ta.trend.SMAIndicator(close=df_hour['Return'], window=50).sma_indicator() # medium term in trading
-# df_hour[f'SMA_{60}'] = ta.trend.SMAIndicator(close=df_hour['Return'], window=60).sma_indicator()
-# df_hour[f'SMA_{100}'] = ta.trend.SMAIndicator(close=df_hour['Return'], window=100).sma_indicator()
-# df_hour[f'SMA_{200}'] = ta.trend.SMAIndicator(close=df_hour['Return'], window=200).sma_indicator() # long term in trading
-
-# # Exponential Moving Average (EMA) (Rolling Window set as typically done in trading) - da mas peso a los precios recientes
-# df_hour[f'EMA_{12}'] = ta.trend.EMAIndicator(close=df_hour['Return'], window=12).ema_indicator()
-# df_hour[f'EMA_{26}'] = ta.trend.EMAIndicator(close=df_hour['Return'], window=26).ema_indicator()
-
-# # Rate of Change (ROC, over N days)
-# df_hour[f'ROC_{12}'] = ta.momentum.ROCIndicator(close=df_hour['Return'], window=12).roc() # as done in trading for momentum
-# df_hour[f'ROC_{50}'] = ta.momentum.ROCIndicator(close=df_hour['Return'], window=50).roc()
-
-# #! Review if this is useful
-# # Relative Strength Index (RSI, N-day window)
-# df_hour[f'RSI_{5}'] = ta.momentum.RSIIndicator(close=df_hour['Return'], window=5).rsi() # rapid momentum changes
-# df_hour[f'RSI_{7}'] = ta.momentum.RSIIndicator(close=df_hour['Return'], window=7).rsi() # rapid momentum changes
-# df_hour[f'RSI_{14}'] = ta.momentum.RSIIndicator(close=df_hour['Return'], window=14).rsi() # standard momentum
-
 
 # Drop missing values generated by rolling calculations
 df_hour.dropna(inplace=True)
@@ -129,8 +107,8 @@ print("Infinity values per column:\n", df_hour.isin([np.inf, -np.inf]).sum())
 
 
 # Save the dataset with metrics for the selected hour
-df_hour.to_csv(f'data/ta_metrics/new_price_metrics_hour_{hour_to_predict}.csv', index=False)
+df_hour.to_csv(f'data/ta_metrics/0401_price_metrics_hour_{hour_to_predict}.csv', index=False)
 # df_hour.to_csv(f'data/ta_metrics/new_return_metrics_hour_{hour_to_predict}.csv', index=False)
 
 # Output message
-print(f"Metrics for hour {hour_to_predict} calculated and saved to 'data/ta_metrics/new_metrics_hour_{hour_to_predict}.csv'.")
+print(f"Metrics for hour {hour_to_predict} calculated and saved to 'data/ta_metrics/0401_metrics_hour_{hour_to_predict}.csv'.")
